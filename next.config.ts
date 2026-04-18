744af0b4e8d7bb16466bbd057b46748de19a822e
KikoEnakTau <theo.niomba@gmail.com>
2026-04-18 17:46:13 +0700

    feat: add solana program, pino-js, scripts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const API_URL = process.env.API_URL ?? "http://localhost:5000";
+const API_URL = process.env.API_URL ?? "http://localhost:3001";
 
 const nextConfig: NextConfig = {
   async rewrites() {
